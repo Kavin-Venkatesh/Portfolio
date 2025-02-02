c202d09d7a4f121ec368818f642d32eeb1f481d1
Kavin-Venkatesh <kavin.al22@bitsathy.ac.in>
2025-02-02 22:50:12 +0530

    Changes in Team members image and personal Image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { useState } from 'react';
-import Logo from './Assets/portfolio.jpg';
+import Logo from './Assets/photo.webp';
+import Image from './Assets/portfolio.jpg';
+import AdeshImage from './Assets/adeshImage.jpg';
+import SaranImage from './Assets/saranImage.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot , faPhone , faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
@@ -11,6 +14,7 @@ function App() {
   const skills = [
     { name: 'JavaScript', percentage: 90 },
     { name: 'React', percentage: 80 },
+    { name: 'Java', percentage: 70 },
     { name: 'CSS', percentage: 85 },
     { name: 'HTML', percentage: 95 },
     { name: 'Python', percentage: 70 },
@@ -20,21 +24,22 @@ function App() {
   ];
 
   const projects = [
-    {name : "Melanoma Cancer Prediction" , year : '2022-2023', projectDescription : 'This project involves developing a Convolutional Neural Network (CNN) model designed to predict melanoma, a type of skin cancer, based on images of skin lesions. The model is trained on a dataset of labeled images to recognize patterns and features indicative of melanoma. The goal is to create a tool that can assist medical professionals in diagnosing skin cancer more accurately and efficiently by analyzing image data, thus improving early detection and treatment outcomes.'},
-    {name : "Haxguz - An event management System", year : '2022-2023' ,projectDescription : ' Haxguz is a web application designed for managing and organizing events. Users can create and customize events, invite participants, and monitor event details through an intuitive interface. The application aims to streamline event management processes and provide an efficient platform for both organizers and attendees to interact and manage event-related activities.' },
-    {name : 'Consus - A Redefined Comiplation Platform', year : '2023-2024', projectDescription : ' Consus is a web-based platform focused on assessing student performance in programming languages. It provides tools for creating and managing programming tests, running code submissions, and evaluating results. The platform supports various programming languages and offers features for detailed analysis of student code, including automated grading and feedback.' }
+    {name : "Melanoma Cancer Prediction" , year : '2022-2023', StackUsed : ' React JS , Flask , CNN', Role : 'Deep Learning Model Developer',  projectDescription : 'This project involves developing a Convolutional Neural Network (CNN) model designed to predict melanoma, a type of skin cancer, based on images of skin lesions. The model is trained on a dataset of labeled images to recognize patterns and features indicative of melanoma. The goal is to create a tool that can assist medical professionals in diagnosing skin cancer more accurately and efficiently by analyzing image data, thus improving early detection and treatment outcomes.'},
+    {name : "Haxguz - An event management System", year : '2022-2023' ,StackUsed : ' React JS , Express Js , Mongo DB', Role : 'Front End Developer', projectDescription : ' Haxguz is a web application designed for managing and organizing events. Users can create and customize events, invite participants, and monitor event details through an intuitive interface. The application aims to streamline event management processes and provide an efficient platform for both organizers and attendees to interact and manage event-related activities.' },
+    {name : 'Consus - A Redefined Comiplation Platform', year : '2023-2024',StackUsed : ' React JS , Express Js , Mongo DB', Role : 'Front End Developer',  projectDescription : ' Consus is a web-based platform focused on assessing student performance in programming languages. It provides tools for creating and managing programming tests, running code submissions, and evaluating results. The platform supports various programming languages and offers features for detailed analysis of student code, including automated grading and feedback.' },
+    {name :'BCC - BIT Career Connect ' , year : '2023 - 2024' ,StackUsed : 'Vue JS , Express JS , MongoDB', Role : 'Full Stack Developer', projectDescription : 'A portal for students to upload, manage, and track job and internship offers with features like submission, administrative approval, document verification, and export functionality.'}
   ];
 
   const members = [
-    {name: 'Adesh S S', role: 'Full Stack Developer', quote: "The only way to do great work is to love what you do. – Steve Jobs", src: Logo},
-    {name: 'Dinesh P R', role: 'Full Stack Developer', quote: "Strive for progress, not perfection", src: Logo},
-    {name: 'Saran S M', role: 'Full Stack Developer', quote: "Trade what you see, not what you think.", src: Logo}
+    {name: 'Adesh S S', role: 'Full Stack Developer', quote: "The only way to do great work is to love what you do. – Steve Jobs", src: AdeshImage},
+    {name: 'Dinesh P R', role: 'Full Stack Developer', quote: "Strive for progress, not perfection", src: Image},
+    {name: 'Saran S M', role: 'Full Stack Developer', quote: "Trade what you see, not what you think.", src: SaranImage},
   ];
 
   const details = [
-    {year : '2019 - 2020', qualification : 'SSLC' ,location :"Tagore Matric Higher Secondary School", description : 'During the SSLC (Secondary School Leaving Certificate) examination conducted by the State Government, I achieved a total score of 486 out of 500. This accomplishment reflects a strong academic performance across all subjects, demonstrating proficiency and dedication in my studies.', status : 'Completed' },
-    {year : '2021 - 2022', qualification : 'HSC',location :"Tagore Matric Higher Secondary School", description : ' In the Higher Secondary Certificate (HSC) examination, I obtained a commendable score of 550. This result highlights my academic excellence in higher secondary education, where I focused on advanced subjects and developed specialized knowledge in my chosen fields of study.', status : 'Completed' },
-    {year : '2022 - 2026', qualification : 'Bachelor of Technology - AIML', location:'Bannari Amman Institute of Technology',description :' This program involves an in-depth study of AI technologies, machine learning algorithms, and data science techniques. My academic journey is marked by a strong commitment to understanding and applying advanced AI concepts, as evidenced by my current CGPA of 7.99.', status : 'Ongoing'}
+    {year : '2019 - 2020', qualification : 'SSLC' , percentage :'97.2' ,location :"Tagore Matric Higher Secondary School", description : 'During the SSLC (Secondary School Leaving Certificate) examination conducted by the State Government, I achieved a total score of 486 out of 500. This accomplishment reflects a strong academic performance across all subjects, demonstrating proficiency and dedication in my studies.', status : 'Completed' },
+    {year : '2021 - 2022', qualification : 'HSC', percentage : '91.66',location :"Tagore Matric Higher Secondary School", description : ' In the Higher Secondary Certificate (HSC) examination, I obtained a commendable score of 550. This result highlights my academic excellence in higher secondary education, where I focused on advanced subjects and developed specialized knowledge in my chosen fields of study.', status : 'Completed' },
+    {year : '2022 - 2026', qualification : 'Bachelor of Technology - AIML', percentage : '8.09 CGPA', location:'Bannari Amman Institute of Technology',description :' This program involves an in-depth study of AI technologies, machine learning algorithms, and data science techniques. My academic journey is marked by a strong commitment to understanding and applying advanced AI concepts, as evidenced by my current CGPA of 8.09.', status : 'Ongoing'}
   ]
   const handleSetActiveLink = (link) => {
     setActiveLink(link);
@@ -99,7 +104,7 @@ function App() {
           <div className='AboutDetails'> <p className='Question'>Phone</p></div>
           <div className='AboutRightDetails'><p className='Answer'>+91 7904863284</p></div>
           </div>
-          <button className='DownloadCV'>
+          <button className='DownloadCV' onClick={() => window.open('https://drive.google.com/file/d/1SNnX1WPmvHGKPDQg10cgjnNTRZHahR2w/view?usp=drive_link' , '_blank')}>
             <p className='downloadText'>DOWNLOAD CV</p>
           </button>
         </div>
@@ -114,6 +119,7 @@ function App() {
            <h3 className='cardTitle'>{detail.qualification} <span className='cardYear'>({detail.year})</span></h3>
            <h2 className='cardTitle'>{detail.location}</h2>
            <h3 className='cardTitle'>Status : <span id='status'>{detail.status}</span></h3>
+           <h3 className='cardTitle'> Percentage :<span className='cardYear'>{detail.percentage}</span></h3>
            <p className='cardDescription'>{detail.description}</p>
          </div>
         ))}
@@ -151,6 +157,8 @@ function App() {
             <div  key = {index} className = "ProjectCard">
               <h3 className='cardYear'> {project.year}</h3>
               <h3 className='cardTitle'>{project.name}</h3>
+              <h3 className='cardTitle'>Technologies Used: {project.StackUsed}</h3>
+              <h3 className='cardTitle'> Role : {project.Role}</h3>
               <p className='cardDescription'>{project.projectDescription}</p>
             </div>
           ))}
